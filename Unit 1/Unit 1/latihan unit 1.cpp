2d258db207f3807c8398fb2c64e5bb790b119fa8
Ihsenpai <151743371+Ihsenpai@users.noreply.github.com>
2024-03-05 12:48:28 +0700

    latihan unit 1.cpp

--- Unit 1/Unit 1/latihan unit 1.cpp
+++ Unit 1/Unit 1/latihan unit 1.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
 using namespace std;
-
-int menu;
-int length_int, time_int, speed_int; 
-float length, time, speed;
-
+ int menu;
+    int jrk, waktu, cepatan;
 int main() {
+    
+   
+
+    
     cout << "Pilih Operasi yang digunakan" << endl;
     cout << "1. Menghitung Kecepatan:" << endl;
     cout << "2. Menghitung Jarak:" << endl;
@@ -14,32 +15,30 @@ int main() {
     cout << "Masukkan nomor : ";
     cin >> menu;
 
+    
     cout << "Masukkan nilai jarak: ";
-    cin >> length_int;
+    cin >> jrk;
     cout << "Masukkan nilai waktu: ";
-    cin >> time_int;
+    cin >> waktu;
     cout << "Masukkan nilai kecepatan: ";
-    cin >> speed_int;
+    cin >> cepatan;
 
-    length = static_cast<float>(length_int);
-    time = static_cast<float>(time_int);
-    speed = static_cast<float>(speed_int);
-
+ 
     switch (menu) {
         case 1:
-            speed = length / time; 
-            cout << "Kecepatan: " << speed << endl;
+            cepatan = static_cast<float>(jrk) / static_cast<float>(waktu);
+            cout << "Kecepatan: " << cepatan << endl;
             break;
         case 2:
-            length = speed * time;
-            cout << "Jarak: " << length << endl;
+            jrk = static_cast<float>(cepatan) * static_cast<float>(waktu);
+            cout << "Jarak: " << jrk << endl;
             break;
         case 3:
-            time = length / speed; 
-            cout << "Waktu: " << time << endl;
+            waktu = static_cast<float>(jrk) / static_cast<float>(cepatan);
+            cout << "Waktu: " << waktu << endl;
             break;
         default:
-            cout << "Pilihan tidak valid" << endl;
+            cout << "Pilihan tidak valid." << endl;
     }
 
     return 0;

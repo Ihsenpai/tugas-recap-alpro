c64b9c9f1cbd2006a95315d33604f8c2249f4af7
Ihsenpai <151743371+Ihsenpai@users.noreply.github.com>
2024-03-05 12:37:52 +0700

    Update latihan unit 1.cpp

--- Unit 1/Unit 1/latihan unit 1.cpp
+++ Unit 1/Unit 1/latihan unit 1.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 
 using namespace std;
- int menu;
-    int length, time, speed;
-int main() {
-    
-   
 
-    
+int menu;
+int length_int, time_int, speed_int; 
+float length, time, speed;
+
+int main() {
     cout << "Pilih Operasi yang digunakan" << endl;
     cout << "1. Menghitung Kecepatan:" << endl;
     cout << "2. Menghitung Jarak:" << endl;
@@ -15,26 +14,28 @@ int main() {
     cout << "Masukkan nomor : ";
     cin >> menu;
 
-    
     cout << "Masukkan nilai jarak: ";
-    cin >> length;
+    cin >> length_int;
     cout << "Masukkan nilai waktu: ";
-    cin >> time;
+    cin >> time_int;
     cout << "Masukkan nilai kecepatan: ";
-    cin >> speed;
+    cin >> speed_int;
 
- 
+    length = static_cast<float>(length_int);
+    time = static_cast<float>(time_int);
+    speed = static_cast<float>(speed_int);
+
     switch (menu) {
         case 1:
-            speed = static_cast<float>(length) / static_cast<float>(time);
+            speed = length / time; 
             cout << "Kecepatan: " << speed << endl;
             break;
         case 2:
-            length = static_cast<float>(speed) * static_cast<float>(time);
+            length = speed * time;
             cout << "Jarak: " << length << endl;
             break;
         case 3:
-            waktu = static_cast<float>(length) / static_cast<float>(speed);
+            time = length / speed; 
             cout << "Waktu: " << time << endl;
             break;
         default:
